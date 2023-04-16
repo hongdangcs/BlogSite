561aa5662e951cd3aeb7455655503a36d54c5de6
hongdangcseiu <hongdangcseiu@gmail.com>
2023-04-17 00:23:08 +0700

    navbar

---
+++ routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"BlogSite/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func PublicRoutes(g *gin.RouterGroup) {
+
+	g.GET("/login", controllers.LoginGetHandler())
+	g.POST("/login", controllers.LoginPostHandler())
+	g.GET("/", controllers.IndexGetHandler())
+
+}
+
+func PrivateRoutes(g *gin.RouterGroup) {
+
+	g.GET("/dashboard", controllers.DashboardGetHandler())
+	g.GET("/logout", controllers.LogoutGetHandler())
+
+}
